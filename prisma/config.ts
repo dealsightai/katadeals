a793cfbcb26ae47f7be37b50ed34b6bec7ff563e
Kata <dealsightai@gmail.com>
2026-03-14 17:29:02 -0700

    Update config.ts

--- prisma/config.ts
+++ prisma/config.ts
@@ -1,7 +1,8 @@
-import path from 'path'
 import { defineConfig } from 'prisma/config'
 
 export default defineConfig({
   earlyAccess: true,
-  schema: path.join('prisma', 'schema.prisma'),
+  datasource: {
+    url: process.env.DATABASE_URL,
+  },
 })
